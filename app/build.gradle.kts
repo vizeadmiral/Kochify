7d01a9ebf334c40b1382e5d5a1aeac0ca33f5138
vizeadmiral <136725187+vizeadmiral@users.noreply.github.com>
2026-07-24 16:24:06 +0200

    Fix YouTube challenge downloads in Kochify 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "de.kochify.music"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.1.1"
     }
 
     buildFeatures {
