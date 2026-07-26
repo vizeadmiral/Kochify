d1674459ebc9e16b68d198496ad575f7a42c55fe
vizeadmiral <136725187+vizeadmiral@users.noreply.github.com>
2026-07-26 23:32:23 +0200

    Bump Kochify to version 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "de.kochify.music"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.2.0"
+        versionCode = 10
+        versionName = "1.2.1"
     }
 
     buildFeatures {
